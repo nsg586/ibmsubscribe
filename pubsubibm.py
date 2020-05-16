0e48fdd33a9213952aac686279b922868daa90f6
rachuriharish23 <50615859+rachuriharish23@users.noreply.github.com>
2020-05-16 13:06:42 +0530

    Add files via upload

---
+++ pubsubibm.py
@@ -0,0 +1,72 @@
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+import random
+'''Organization ID
+gpb305
+Device Type
+testingcode
+Device ID
+testing123
+Authentication Method
+use-token-auth
+Authentication Token
+testing1234
+'''
+#Provide your IBM Watson Device Credentials
+organization = "gpb305"
+deviceType = "testingcode"
+deviceId = "testing123"
+authMethod = "token"
+authToken = "testing1234"
+# Initialize GPIO
+
+
+# Initialize the device client.
+T=0
+H=0
+
+def myCommandCallback(cmd):
+        print("Command received: %s" % cmd.data)        
+        if cmd.command == "setInterval":
+                if 'interval' not in cmd.data:
+                        print("Error - command is missing required information: 'interval'")
+                else:
+                        interval = cmd.data['interval']
+        elif cmd.command == "print":
+                if 'message' not in cmd.data:
+                        print("Error - command is missing required information: 'message'")
+                else:
+                        print(cmd.data['message'])
+
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+	#..............................................
+	
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+
+while True:
+        T=50;
+        H=32;
+        #Send Temperature & Humidity to IBM Watson
+        data = { 'Temperature' : T, 'Humidity': H }
+        #print data
+        def myOnPublishCallback():
+            print ("Published Temperature = %s C" % T, "Humidity = %s %%" % H, "to IBM Watson")
+
+        success = deviceCli.publishEvent("DHT11", "json", data, qos=0, on_publish=myOnPublishCallback)
+        if not success:
+            print("Not connected to IoTF")
+        time.sleep(1)
+        
+        deviceCli.commandCallback = myCommandCallback
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
